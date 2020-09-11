05e12c6eec074f8f7a3216a9f259a97e2cfdc7ac
Akhdanpangestu <akhdanpw13@gmail.com>
2020-09-11 10:07:55 +0700

    JurnalSatu.java

---
+++ Praktikum 2/JurnalSatu.java
@@ -0,0 +1,15 @@
+public class JurnalSatu {
+	public static void main(String[] asrgs) {
+		
+		int a = 10;
+		int b = 20;
+		int c = a * b;
+
+		System.out.println("Nama      : Akhdan Pangestuaji W");
+		System.out.println("Panggilan : Akhdan");
+		System.out.println("NIM       : 6706190008");
+		System.out.println("Kelas     : 43-02");
+		System.out.println("Hasil dari " + a + " * " + b + " = " + c);
+
+	}
+}
